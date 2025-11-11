0a2c9767b725876f6cd5faf01bcbd3173cda50f5
Jason Moore <jason.moore@fias.wa.edu.au>
2025-11-11 14:00:57 +0800

    :sparkles: Enhance dojostart.cmd functionality
    - Added commands to install the MSI package.
    - Updated the script to open the installation log file.
    - Open the Program Files folder to verify that the installation was successful.

--- ep3/dojostart.cmd
+++ ep3/dojostart.cmd
@@ -1,3 +1,8 @@
 REM Open the Dojo folder and Programs and Features
 start c:\dojo
 start appwiz.cpl
+
+REM Install the MSI
+msiexec /i "C:\dojo\dojo-ep2.msi" /l*v C:\dojo\dojo-ep2-install.txt
+start c:\dojo\dojo-ep2-install.txt
+explorer.exe "C:\Program Files (x86)"
