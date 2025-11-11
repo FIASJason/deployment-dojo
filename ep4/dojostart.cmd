b0eac4f85a9d9707e8667209c7da5a86680c42f6
Jason Moore <jason.moore@fias.wa.edu.au>
2025-11-11 14:47:07 +0800

    :sparkles: Add initial files for Deployment Dojo Episode 4
    - Create `dojo.wsb` for configuration settings
    - Add `dojostart.cmd` for installation commands
    - Introduce `ep4.txt` for episode description
    - Implement `ep4.wxs` for WiX package configuration

---
+++ ep4/dojostart.cmd
@@ -0,0 +1,8 @@
+REM Open the Dojo folder and Programs and Features
+start c:\dojo
+start appwiz.cpl
+
+REM Install the MSI
+msiexec /i "C:\dojo\ep4.msi" /l*v C:\dojo\dojo-ep4-install.txt
+start c:\dojo\dojo-ep4-install.txt
+explorer.exe "C:\Program Files (x86)"
