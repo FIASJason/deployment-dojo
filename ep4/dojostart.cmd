6716c0b04b1f21527d57f27c96c39e135c4bfe17
Jason Moore <jason.moore@fias.wa.edu.au>
2025-11-11 14:55:59 +0800

    :art: Update MSI path in dojostart.cmd
    - Changed the MSI installation path from `C:\dojo\ep4.msi` to `C:\dojo\v0.9\ep4.msi`
    - Ensured the script points to the correct version for installation
    - Allows for testing of upgrades

--- ep4/dojostart.cmd
+++ ep4/dojostart.cmd
@@ -3,6 +3,6 @@ start c:\dojo
 start appwiz.cpl
 
 REM Install the MSI
-msiexec /i "C:\dojo\ep4.msi" /l*v C:\dojo\dojo-ep4-install.txt
+msiexec /i "C:\dojo\v0.9\ep4.msi" /l*v C:\dojo\dojo-ep4-install.txt
 start c:\dojo\dojo-ep4-install.txt
 explorer.exe "C:\Program Files (x86)"
