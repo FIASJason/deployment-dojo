ef7448617d12f972a668c4458fa26805f61eaa32
Jason Moore <jason.moore@fias.wa.edu.au>
2025-11-12 12:27:20 +0800

    :fire: Remove unused files and update project structure
    - Convert `HelloDojo` to .NET Framework.
    ` Add reference to the .NET Framework application to the `ep9.wixproj` file.
    - Deleted `ep9a.txt` and `ep9b.txt` as they are no longer needed.
    - Updated `dojo.wsb` to reflect new folder structure.
    - Added `App.config` and `AssemblyInfo.cs` for `HelloDojo`.
    - Created new `hellodojo.sln` and updated `helloDojo.csproj`.
    - Added `dojostart.cmd` for tool initialization.

--- ep9/helloDojo/Program.cs
+++ ep9/helloDojo/Program.cs
@@ -1,4 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Deployment Dojo!");
-Console.Write("Press the any key to close... ");
-Console.ReadKey();
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloDojo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Deployment Dojo!");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
