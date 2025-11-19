dafe3841029bd12b21fb411a90d4ca3dac62e0bc
Jason Moore <jason.moore@fias.wa.edu.au>
2025-11-19 10:25:13 +0800

    🎉 Initialize .NET Framework console application
    ✨ Added `Program.cs` with a `Main` method to print "Hello, Dojo 14".
    🔧 Added `App.config` to define runtime settings for `.NETFramework,Version=v4.7.2`.
    🔧 Added `ConsoleApp1.csproj` to define project structure, build settings, and references.
    📝 Added `AssemblyInfo.cs` to include assembly metadata such as title, version, and GUID.

---
+++ ep14/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿// ConsoleApp1
+
+namespace ConsoleApp1
+{
+    #region Using Directives
+    using System;
+    #endregion
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Dojo 14");
+            ;
+            Console.ReadKey();
+        }
+    }
+}
