e1c7db81ae4897d93f5422903c1a807f2bd74f57
Jason Moore <jason.moore@fias.wa.edu.au>
2025-11-19 11:06:33 +0800

    🎉 Initialize .NET Framework console app project
    ✨ Added `Program.cs` with a `Main` method to output "Hello, dojo episode 15!" to the console.
    🔧 Added `App.config` to define runtime settings targeting `.NETFramework,Version=v4.7.2`.
    🔧 Added `Ep15Program.csproj` to configure build settings, references, and project structure.
    🔧 Added `Ep15Program.sln` to define the Visual Studio solution structure.
    📝 Added `AssemblyInfo.cs` to include assembly metadata such as title, version, and GUID.

---
+++ ep15/Ep15Program/Program.cs
@@ -0,0 +1,17 @@
+﻿// Ep15Program
+
+namespace Ep15Program
+{
+    #region Using Directives
+    using System;
+    #endregion
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, dojo episode 15!");
+            Console.ReadKey();
+        }
+    }
+}
