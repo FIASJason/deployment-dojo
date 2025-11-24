d0cf0ce5c022bbb246cf5d27a57473e27ab89787
Jason Moore <jason.moore@fias.wa.edu.au>
2025-11-24 15:46:20 +0800

    :art: Fix typo in initialization message
    - Corrected "dev environement" to "dev environment"

--- build.cmd
+++ build.cmd
@@ -2,7 +2,7 @@
 
 pushd
 
-echo Initializing dev environement
+echo Initializing dev environment
 
 for /f "usebackq delims=" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -version [17.0^,18.0^) -property installationPath`) do (
   call "%%i\Common7\Tools\vsdevcmd.bat" -no_logo
