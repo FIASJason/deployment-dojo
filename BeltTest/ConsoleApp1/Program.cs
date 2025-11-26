5375106498c385ba54330203e39289ffc613bba6
Jason Moore <jason.moore@fias.wa.edu.au>
2025-11-26 08:00:49 +0800

    ✨ Add real-time file monitoring to `Main` method
    - ✨ Added `GetCountFilePath()` to retrieve the file path for the service's output file, with fallback logic.
    - 🧵 Introduced a loop in `Main` to monitor and display changes in the file's content in real-time.
    - 🔧 Added error handling for file reading operations.
    - 🧵 Used `Thread.Sleep(1000)` to reduce CPU usage during the monitoring loop.
    - ➕ Added `System.IO` and `System.Threading` namespaces for file handling and threading functionality.

--- BeltTest/ConsoleApp1/Program.cs
+++ BeltTest/ConsoleApp1/Program.cs
@@ -4,11 +4,29 @@ namespace ConsoleApp1
 {
     #region Using Directives
     using System;
+    using System.IO;
+    using System.Threading;
     using Microsoft.Win32;
     #endregion
 
     internal class Program
     {
+        static string GetCountFilePath()
+        {
+            try
+            {
+                var path = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\BeltTest", "CountFilePath", null) as string;
+
+                return Path.GetFullPath(path);
+            }
+            catch
+            {
+                // ignored
+            }
+
+            return Path.Combine(AppContext.BaseDirectory, "WindowsService1.txt");
+        }
+
         static void Main(string[] args)
         {
             var edition  = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\BeltTest", "Edition",  null);
@@ -16,6 +34,32 @@ namespace ConsoleApp1
 
             Console.WriteLine($"Welcome to the {edition} ConsoleApp1.");
             Console.WriteLine($"  Registered to our {customer} customer");
+
+            // Episode 25 - Reading from the Service's Output File
+            var path    = GetCountFilePath();
+            var oldText = string.Empty;
+
+            while (!Console.KeyAvailable)
+            {
+                string text;
+
+                try
+                {
+                    text = File.ReadAllText(path);
+                }
+                catch (Exception e)
+                {
+                    text = e.Message;
+                }
+
+                if (text != oldText)
+                {
+                    Console.WriteLine(text);
+                    oldText = text;
+                }
+
+                Thread.Sleep(1000);
+            }
         }
     }
 }
